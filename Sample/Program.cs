9f0ec946b585c0da3870d99460a6c705017b9609
David Fowler <davidfowl@gmail.com>
2022-10-27 20:14:01 -0700

    More samples

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.CodeAnalysis.CSharp;
 
 var compilation = CSharpCompilation.Create("something",
     syntaxTrees: new[] { CSharpSyntaxTree.ParseText(@"
+using Microsoft.AspNetCore.Mvc;
+
 public class NotAPlugin
 {
 }
@@ -13,6 +15,25 @@ public class Plugin2 : IPlugin { }
 
 public interface IPlugin { }
 
+public class MyController : ControllerBase
+{
+    [HttpGet(""/hello/{name}"")]
+    public IActionResult Get(string name) => Ok(""Hello World"");
+}
+
+[Authorize]
+public class AuthController : ControllerBase  { }
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    public class ControllerBase { }
+
+    public class AuthorizeAttribute : System.Attribute { }
+    public class HttpGetAttribute : System.Attribute 
+    { 
+       public HttpGetAttribute(string path) { }
+    }
+}
 ") },
     references: new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
     });
@@ -21,12 +42,50 @@ var metadataLoadContext = new MetadataLoadContext(compilation);
 
 // Resolve the type by name
 var pluginType = metadataLoadContext.ResolveType("IPlugin");
+var controllerType = metadataLoadContext.ResolveType("Microsoft.AspNetCore.Mvc.ControllerBase");
 
-// Find all plugin types
+Console.WriteLine("Plugins");
+Console.WriteLine();
+
+// Find all types with a base class or interface
+
 foreach (var t in metadataLoadContext.Assembly.GetTypes())
 {
     if (!t.Equals(pluginType) && pluginType.IsAssignableFrom(t))
     {
-        Console.WriteLine(t);
+        Console.WriteLine($"- {t}");
+    }
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Controllers");
+Console.WriteLine();
+
+
+foreach (var t in metadataLoadContext.Assembly.GetTypes())
+{
+
+    if (!t.Equals(controllerType) && controllerType.IsAssignableFrom(t))
+    {
+        Console.WriteLine($"- {t}");
+
+        foreach (var m in t.GetMethods())
+        {
+            Console.WriteLine($"    {m}");
+        }
+    }
+}
+Console.WriteLine();
+
+var attribute = metadataLoadContext.ResolveType("Microsoft.AspNetCore.Mvc.AuthorizeAttribute");
+
+Console.WriteLine("Types with authorize attribute");
+
+foreach (var t in metadataLoadContext.Assembly.GetTypes())
+{
+    if (t.CustomAttributes.Any(c => c.AttributeType.Equals(attribute)))
+    {
+        Console.WriteLine(@$" - {t}");
     }
 }
